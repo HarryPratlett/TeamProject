9e6e5e6ffc36b74ecac8ec78ae977301a2b10d58
hjp705 <hjp705@student.bham.ac.uk>
2019-02-01 14:51:24 +0000

    Button images working and rectangle works

--- project/src/com/myst/GUI/Main.java
+++ project/src/com/myst/GUI/Main.java
@@ -8,11 +8,14 @@ import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.lwjgl.opengl.GL;
 import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.util.Arrays;
 
 import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
 import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+
 
 public class Main {
 
@@ -34,38 +37,28 @@ public class Main {
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glClearColor(0f,0f,0f, 0f);
 
-
-        float[] vertices = new float[] {
-                -0.5f, 0.5f, 0f, /*0*/  0.5f, 0.5f, 0f, /*1*/    0.5f, -0.5f, 0f, /*2*/
-                -0.5f, -0.5f, 0f/*3*/
-        };
-
-        float[] textureDocks = new float[] {
-                0f, 0f,   1, 0f,  1f, 1f,
-                0f, 1f
-        };
-
-        int[] indices = new int[] {
-                0,1,2,
-                2,3,0
-        };
-
         Shader shader = new Shader("assets/shader");
 
-        Model model = new Model(vertices, textureDocks, indices);
+
 
 
         while (!window.shouldClose()){
             window.update();
 
-            renderGUI(model, shader, new Matrix4f().scale(0.3f));
+            renderGUI(shader, new Matrix4f().scale(0.3f));
             window.swapBuffers();
+            try {
+                Thread.sleep(  100);
+            }
+            catch  (InterruptedException e) {
+                System.exit(1);
+            }
         }
 
     }
 
 
-    public static void renderImage(Shader shader, Texture texture, int x, int y, Matrix4f scale, Model model){
+    public static void renderImage(Shader shader, Texture texture, float x, float y, Matrix4f scale, Model model){
         shader.bind();
         texture.bind(0);
         Matrix4f target = new Matrix4f();
@@ -81,23 +74,49 @@ public class Main {
 
     }
 
-    public static void renderGUI(Model model, Shader shader, Matrix4f scale) {
+    public static void renderGUI(Shader shader, Matrix4f scale) {
+
+        final float[] baseVertices = new float[] {
+                -1f, 0.5f, 0f, /*0*/  1f, 0.5f, 0f, /*1*/    1f, -0.5f, 0f, /*2*/
+                -1f, -0.5f, 0f/*3*/
+        };
+
+        float[] textureDocks = new float[] {
+                0f, 0f,   1, 0f,  1f, 1f,
+                0f, 1f
+        };
+
+        int[] indices = new int[] {
+                0,1,2,
+                2,3,0
+        };
+
+
         Texture[] menuTextures = new Texture[]  {
                 new Texture("assets/resume_button.png"), new Texture("assets/controls_button.png"),
                 new Texture("assets/settings_button.png"), new Texture("assets/exit_button.png")
         };
-        int y = 2;
+        float y = 2.25f;
         for (Texture t : menuTextures)   {
-            renderImage(shader, t, 0, y, scale, model);
-            //addButton(x, y, t); //needs changing to be positions of button
-            y -= 1;
+
+            float[] vertices = Arrays.copyOf(baseVertices, baseVertices.length);
+            vertices[0] *= t.getWidth()*0.009;
+            vertices[3] *= t.getWidth()*0.009;
+            vertices[6] *= t.getWidth()*0.009;
+            vertices[9] *= t.getWidth()*0.009;
+
+            Model model = new Model(vertices, textureDocks, indices);
+            renderImage(shader, t, 0f, y, scale, model);
+
+            addButton(0, y, t); //needs changing to be positions of button
+            y += (-1.5f);
         }
 
 
     }
 
-    public static Rectangle addButton(int x, int y, Texture texture) {
-        Rectangle bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
+    public static Rectangle2D.Float addButton(float x, float y, Texture texture) {
+        Rectangle2D.Float bounds = new Rectangle2D.Float(x, y, texture.getWidth(), texture.getHeight());
         return bounds;
     }
 
