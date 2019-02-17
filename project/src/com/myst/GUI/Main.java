51d040561efdd457c2a33e61d23b1183d846bad0
hjp705 <hjp705@student.bham.ac.uk>
2019-02-17 11:22:11 +0000

    Controls functional, image vertices may need tweaking

--- project/src/com/myst/GUI/Main.java
+++ project/src/com/myst/GUI/Main.java
@@ -15,9 +15,7 @@ import java.util.List;
 import java.awt.geom.Rectangle2D;
 import java.util.Arrays;
 
-import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_1;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwSetCursorPosCallback;
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
 import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
@@ -26,6 +24,23 @@ import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
 public class Main {
 
     HashMap<Rectangle2D.Float, String> buttons = new HashMap<>();
+
+    final float[] baseVertices = new float[] {
+            -1f, 0.5f, 0f, /*0*/  1f, 0.5f, 0f, /*1*/    1f, -0.5f, 0f, /*2*/
+            -1f, -0.5f, 0f/*3*/
+    };
+
+    float[] textureDocks = new float[] {
+            0f, 0f,   1, 0f,  1f, 1f,
+            0f, 1f
+    };
+
+    int[] indices = new int[] {
+            0,1,2,
+            2,3,0
+    };
+
+    Boolean controls_accessed = false;
 
     public static void main(String[] args){
         Main GUI = new Main();
@@ -58,9 +73,16 @@ public class Main {
         while (!window.shouldClose()){
             window.update();
 
-            GUI.renderGUI(shader);
-            GUI.checkButtons(window);
+            if (GUI.controls_accessed)  {
+                GUI.accessControls(window);
+            }
+            else {
+                glClear(GL_COLOR_BUFFER_BIT);
+                GUI.renderGUI(shader);
+                GUI.checkButtons(window);
+            }
             window.swapBuffers();
+
             try {
                 Thread.sleep(  100);
             }
@@ -90,47 +112,33 @@ public class Main {
 
     public void renderGUI(Shader shader) {
 
-        final float[] baseVertices = new float[] {
-                -1f, 0.5f, 0f, /*0*/  1f, 0.5f, 0f, /*1*/    1f, -0.5f, 0f, /*2*/
-                -1f, -0.5f, 0f/*3*/
-        };
+            Texture[] menuTextures = new Texture[]{
+                    new Texture("assets/resume_button.png"), new Texture("assets/controls_button.png"),
+                    new Texture("assets/settings_button.png"), new Texture("assets/exit_button.png")
+            };
 
-        float[] textureDocks = new float[] {
-                0f, 0f,   1, 0f,  1f, 1f,
-                0f, 1f
-        };
+            float y = 0.55f;
+            for (Texture t : menuTextures) {
+                float[] vertices = Arrays.copyOf(baseVertices, baseVertices.length);
+                vertices[0] *= t.getWidth() * 0.002;
+                vertices[3] *= t.getWidth() * 0.002;
+                vertices[6] *= t.getWidth() * 0.002;
+                vertices[9] *= t.getWidth() * 0.002;
 
-        int[] indices = new int[] {
-                0,1,2,
-                2,3,0
-        };
+                vertices[1] *= t.getHeight() * 0.005;
+                vertices[4] *= t.getHeight() * 0.005;
+                vertices[7] *= t.getHeight() * 0.005;
+                vertices[10] *= t.getHeight() * 0.005;
 
-        Texture[] menuTextures = new Texture[]  {
-                new Texture("assets/resume_button.png"), new Texture("assets/controls_button.png"),
-                new Texture("assets/settings_button.png"), new Texture("assets/exit_button.png")
-        };
-
-        float y = 0.55f;
-        for (Texture t : menuTextures)   {
-            float[] vertices = Arrays.copyOf(baseVertices, baseVertices.length);
-            vertices[0] *= t.getWidth()*0.002;
-            vertices[3] *= t.getWidth()*0.002;
-            vertices[6] *= t.getWidth()*0.002;
-            vertices[9] *= t.getWidth()*0.002;
-
-            vertices[1] *= t.getHeight()*0.005;
-            vertices[4] *= t.getHeight()*0.005;
-            vertices[7] *= t.getHeight()*0.005;
-            vertices[10] *= t.getHeight()*0.005;
-
-            Model model = new Model(vertices, textureDocks, indices);
-            renderImage(shader, t, 0f, y, new Matrix4f(), model);
+                Model model = new Model(vertices, textureDocks, indices);
+                renderImage(shader, t, 0f, y, new Matrix4f(), model);
 
 //            -1 -- 0 -- 1
 
-            this.addButton(0 + vertices[0],  y + vertices[1],  vertices[3] - vertices[0], vertices[1] - vertices[7], t.getPath());
-            y += (-0.35f);
-        }
+                this.addButton(0 + vertices[0], y + vertices[1], vertices[3] - vertices[0], vertices[1] - vertices[7], t.getPath());
+                y += (-0.35f);
+            }
+
     }
 
     public void addButton(float x, float y, float width, float height, String filepath) {
@@ -138,6 +146,7 @@ public class Main {
         Rectangle2D.Float bounds = new Rectangle2D.Float(x, y, width , height);
         buttons.put(bounds, filepath);
     }
+
 
     public void checkButtons(Window window)   {
         for (Rectangle2D.Float b : buttons.keySet())   {
@@ -148,13 +157,17 @@ public class Main {
             if (mouseX >= b.getX() && mouseX <= (b.getX()+b.getWidth()) && mouseY <= b.getY() && mouseY >= (b.getY()-b.getHeight()))  {
                 if (window.getInput().isMouseButtonDown(GLFW_MOUSE_BUTTON_1)){
 
-                    //System.out.println("mouse X : " + mouseX + "button x and x+width: " + b.getX() + " " + (b.getX()+b.getWidth()) + "mousey: " +  mouseY + "bY and bY-height: " + b.getY() + " " + (b.getY()+b.getHeight()));i
                     String buttonName = buttons.get(b);
                     switch(buttonName) {
+                        case "resume_button.png": this.resumeGame();
+                            break;
+                        case "controls_button.png": this.accessControls(window);
+                            controls_accessed = true;;
+                            break;
+                        case "settings_button.png": System.exit(3);
+                            break;
                         case "exit_button.png": this.exitGame();
-                        case "settings_button.png": System.exit(2);
-                        case "controls_button.png": System.exit(3);
-                        case "resume_button.png": System.exit(4);
+                            break;
                      }
                 }
             }
@@ -164,9 +177,26 @@ public class Main {
         }
     }
 
+    public void resumeGame(){}
+    public void accessControls(Window window) {
+        if (controls_accessed) {
+            glClear(GL_COLOR_BUFFER_BIT);
+            Shader shader = new Shader("assets/shader");
+            Texture controlsTexture = new Texture("assets/Keyboard_asset.png");
+            Model model = new Model(baseVertices, textureDocks, indices);
+            Matrix4f scale = new Matrix4f();
+            this.renderImage(shader, controlsTexture, 0, 0, scale, model);
+
+            if (window.getInput().isKeyPressed(GLFW_KEY_ESCAPE)) {
+                controls_accessed = false;
+            }
+        }
+    }
+    public void accessSettings(){}
     public void exitGame()  {
         System.exit(1);
     }
+
 
 
 }
